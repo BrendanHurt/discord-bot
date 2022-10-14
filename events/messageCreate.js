a21e47e5188cf87803e9757ada95b25e0bea39e8
Brendan Hurt <brendan.q.hurt@gmail.com>
2022-10-14 12:41:05 -0700

    Starting to implement new arg system
    For now the new parsing function isn't being called until I change how
    the commands expect to receive arguments. I'm also going to be making
    the command loading function non-recursive so I can put utility
    functions closer to where they're being used.

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -15,3 +15,21 @@ module.exports = (client, message) => {
         console.error(error);
     }
 }
+
+/**
+ * Parses the arguements from the input string. Identifies "groups" of arguments
+ * based on keywords with a leading dash (-). Basically similar to how shells handle args.
+ * @param {string} argString The argument string provided by the user
+ * @return {[]} Returns an array that holds strings for individual arguments
+ *  and arrays for argument groups
+ */
+function parseArgs(argString) {
+    argGroups = argString.split(/ -+/g);
+    args = [];
+    //loop through argGroups & split by spaces
+    for (const group of argGroups) {
+        let arg = group.split(/ +/g);
+        args.push(arg);
+    }
+    return args;
+}
